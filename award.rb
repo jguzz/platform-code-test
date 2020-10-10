0c94f54741c45b1f0502ae74e5bfdb0c47939072
jguzz <joseph.a.guzzardo@gmail.com>
2020-10-09 22:19:03 -0400

    update_quality_switch used to streamline the quality and experation updates for awards currently only gives 6 failures

--- award.rb
+++ award.rb
@@ -10,7 +10,7 @@ class Award
 	end
 
 	#Copied from original update_quality.rb
-		# award changed to self
+	# award changed to self
 	def update_quality 
 		if self.name != 'Blue First' && self.name != 'Blue Compare'
 			if self.quality > 0
@@ -40,21 +40,128 @@ class Award
 		end
 		if self.expires_in < 0
 			if self.name != 'Blue First'
-			if self.name != 'Blue Compare'
-				if self.quality > 0
-				if self.name != 'Blue Distinction Plus'
-					self.quality -= 1
-				end
+				if self.name != 'Blue Compare'
+					if self.quality > 0
+						if self.name != 'Blue Distinction Plus'
+							self.quality -= 1
+						end
+					end
+				else
+					self.quality = self.quality - self.quality
 				end
 			else
-				self.quality = self.quality - self.quality
+				if self.quality < 50
+					self.quality += 1
+				end
 			end
-			else
-			if self.quality < 50
-				self.quality += 1
-			end
+		end
+		# ++++======+++++======+++++
+	end
+
+	def update_quality_switch
+		case self.name
+
+		when 'Blue First'
+			self.blue_first_quality
+			
+		when 'Blue Compare'
+			self.blue_compare_quality
+
+
+		when 'Blue Distinction Plus'
+			self.blue_distinction_plus_quality
+			
+
+		when 'Blue Star'
+			self.blue_star_quality
+			
+			# Other awards...
+		else 
+			self.regular_quality
+		end
+	end
+	
+	
+	
+	def regular_quality
+		self.expires_in -= 1
+		if self.quality > 0 
+			self.quality -= 1 
+		end
+
+		if self.expires_in < 0
+			if self.quality > 0
+				self.quality -= 1
 			end
 		end
 	end
+	
+	#Helper method for all special excluding 'Blue Distinction Plus'
+	def special_quality
+		self.expires_in -= 1
+		
+		if self.quality < 50  
+			self.quality += 1 			
+		end
+	end
+	
+	# increases in quality the closer it is to expiring
+	#Quality increases by 2 when there are 10 or < days left till it expires, and 3 when there are 5 days or <
+	# Quality == 0 when experation hits.
+	def blue_compare_quality
+		self.special_quality
+		case self.expires_in
+		when 7..11
+			if self.quality < 50
+				self.quality += 1
+			end
+		when 1..6
+			if self.quality < 50
+				self.quality += 1
+			end	
+		when self.expires_in < 0
+			self.quality = self.quality - self.quality
+		end
+	end
+	
+	# Quality increases over time
+	def blue_first_quality
+		self.special_quality
+		if self.expires_in < 0
+			if self.quality < 50
+				self.quality += 1
+			end
+		end
+	end
+	
+	# Never decreases in value
+	# Always 80pts
+	def blue_distinction_plus_quality
+		self.quality = 80
+		self.expires_in < 0 ? self.quality = self.quality - self.quality : nil 
+	end
+
+	#  Quality decreses by x2 the amount of pts per day.
+	def blue_star_quality
+		self.special_quality
+		self.quality > 0 ? self.quality -= 1 : nil 
+
+		if self.expires_in < 0
+			if self.quality > 0
+				self.quality -= 1
+			end
+		end
+		
+	end 
 end
 
+
+
+
+
+# blue = Award.new("Blue Star", 10, 15)
+# p blue
+# p blue.quality
+# blue.update_quality
+# p blue.quality
+# blue.update_quality_switch
--- update_quality.rb
+++ update_quality.rb
@@ -2,13 +2,13 @@ require_relative 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    award.update_quality
+    award.update_quality_switch
   end
 end
 
 # Testing
-blue = Award.new("Blue Compare", 10, 10)
+# blue = Award.new("Blue Compare", 10, 10)
 
-p blue
-update_quality([blue])
-p blue
+# p blue
+# update_quality([blue])
+# p blue
